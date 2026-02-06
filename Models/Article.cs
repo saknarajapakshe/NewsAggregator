03a5759de298619032094c4d00c0b09c80cddd29
saknarajapakshe <rajapaksherars.22@uom.lk>
2026-02-06 21:58:14 +0530

    chore: define Article model properties

--- Models/Article.cs
+++ Models/Article.cs
@@ -7,6 +7,12 @@ namespace NewsAggregator.Models
 {
     public class Article
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Url { get; set; } = null!;
+        public string Source { get; set; } = null!;
+        public DateTime PublishedAt { get; set; }
+        public DateTime CreatedAt { get; set;   }
     }
 }
