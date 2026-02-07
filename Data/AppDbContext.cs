73fdfb673d6c18d44143488e6c5c57adffae2789
saknarajapakshe <rajapaksherars.22@uom.lk>
2026-02-07 08:51:35 +0530

    feat: implement initial migration and update AppDbContext for Article model

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,12 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewsAggregator.Models;
 
 namespace NewsAggregator.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+           {
+            modelBuilder.Entity<Article>()
+                .HasIndex(a => a.PublishedAt);
+
+            modelBuilder.Entity<Article>()
+                .HasIndex(a => a.Url)
+                .IsUnique();
+        }
     }
 }
---
+++ Migrations/20260207012612_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsAggregator.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Source = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublishedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_PublishedAt",
+                table: "Articles",
+                column: "PublishedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Url",
+                table: "Articles",
+                column: "Url",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articles");
+        }
+    }
+}
