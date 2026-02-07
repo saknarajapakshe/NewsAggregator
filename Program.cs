63a605a802b64051270f580ea9cfcfdbf15449bc
saknarajapakshe <rajapaksherars.22@uom.lk>
2026-02-07 08:52:14 +0530

    feat: configure DbContext and add HttpClient for news API

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
+using NewsAggregator.Data;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient("NewsClient", client =>
+{
+    client.Timeout = TimeSpan.FromSeconds(10);
+});
+
 
 var app = builder.Build();
 
