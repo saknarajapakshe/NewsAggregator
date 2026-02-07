82893740972b8cb1253f68e51d0ac2ae8dfa45f3
saknarajapakshe <rajapaksherars.22@uom.lk>
2026-02-07 09:45:55 +0530

    feat: implement RssNewsFetcher to fetch news articles from BBC RSS feed

--- Services/Fetchers/RssNewsFetcher.cs
+++ Services/Fetchers/RssNewsFetcher.cs
@@ -1,12 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Xml.Linq;
+using NewsAggregator.Models;
+using NewsAggregator.Services.Interfaces;
 
 namespace NewsAggregator.Services.Fetchers
 {
-    public class RssNewsFetcher
+    public class RssNewsFetcher : INewsFetcher
     {
-        
+        private const string RssUrl = "https://feeds.bbci.co.uk/news/rss.xml";
+
+        private readonly HttpClient _httpClient;
+
+        public RssNewsFetcher(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Article>> FetchAsync(CancellationToken cancellationToken)
+        {
+            var xml = await _httpClient.GetStringAsync(RssUrl, cancellationToken);
+
+            var document = XDocument.Parse(xml);
+
+            return document
+                .Descendants("item")
+                .Select(item => new Article
+                {
+                    Title = (string)item.Element("title")!,
+                    Url = (string)item.Element("link")!,
+                    Description = (string)item.Element("description")!,
+                    Source = "BBC News",
+                    PublishedAt = DateTime.Parse((string)item.Element("pubDate")!)
+                })
+                .ToList();
+        }
     }
 }
